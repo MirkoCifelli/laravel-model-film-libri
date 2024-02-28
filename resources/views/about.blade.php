b36d78ff1de9dac2f9962c81cb1731419f465da8
Mirko Cifelli <146765212+MirkoCifelli@users.noreply.github.com>
2024-02-28 15:42:10 +0100

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title', 'Chi siamo')
+
+@section('main-content')
+<h1>
+    About | Laravel template
+</h1>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('page-title', 'Home')
+
+@section('main-content')
+<h1>
+    Laravel template Welcome
+</h1>
+
+@endsection
